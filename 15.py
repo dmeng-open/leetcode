134e15f9abda5fe7f7e7e488ca72ee0bc8aae9b5
xiaoran-MENG <meng.xiaoran@outlook.com>
2026-08-31 07:24:52 -0500

    15

--- 15.py
+++ 15.py
@@ -1,12 +1,14 @@
 from typing import List
 
+# Time: O(n^2)
+# Space: O(1)
 class Solution:
     def threeSum(self, inputs: List[int]) -> List[List[int]]:
-        inputs.sort()
+        inputs.sort() # O(nlog(n))
         triplets = []
         n = len(inputs)
         i = 0
-        for i in range(n - 2):
+        for i in range(n - 2): # O(n)
             if i > 0 and inputs[i] == inputs[i - 1]:
                 continue
             # 输入值已经排序，如果当前值 > 0，后面的值都 > 0
@@ -14,14 +16,14 @@ class Solution:
                 break
             j = i + 1
             k = n - 1
-            while j < k:
+            while j < k: # O(n)
                 sum = inputs[i] + inputs[j] + inputs[k]
                 if sum > 0:
                     k -= 1
                 elif sum < 0:
                     j += 1
                 else:
-                    triplets.append([inputs[i], inputs[j], inputs[k]])
+                    triplets.append([inputs[i], inputs[j], inputs[k]]) # O(1)
                     while j < k and inputs[j] == inputs[j + 1]:
                         j += 1
                     while j < k and inputs[k] == inputs[k - 1]:
