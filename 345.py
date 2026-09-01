e6bf0a45425b215343a1d6b5009fdde92f4a0815
xiaoran-MENG <meng.xiaoran@outlook.com>
2026-08-31 23:04:02 -0500

    345

---
+++ 345.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverse(self, s: str) -> None:
+        vowels = set("aeiouAEIOU")
+        result = list(s)
+        i, j = 0, len(s) - 1
+        while i < j:
+            while i < j and result[i] not in vowels:
+                i += 1
+            while i < j and not result[j] not in vowels:
+                j -= 1
+            result[i], result[j] = result[j], result[i]
+            i += 1
+            j -= 1
+        return ''.join(result)
