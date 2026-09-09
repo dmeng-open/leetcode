308ddb91c3ff3e0ff148cef56e787200eef5d7bd
xiaoran-MENG <meng.xiaoran@outlook.com>
2026-09-09 00:18:06 -0500

    424

--- sliding-window/424_longest_repeating_character_replacement.py
+++ sliding-window/424_longest_repeating_character_replacement.py
@@ -7,6 +7,7 @@ def character_replacement(s: str, k: int) -> int:
         freq[s[j]] = freq.get(s[j], 0) + 1
         max_freq = max(max_freq, freq[s[j]])
         while j - i + 1 - max_freq > k:
+            freq[s[i]] -= 1
             i += 1
         result = max(result, j - i + 1)
     return result
